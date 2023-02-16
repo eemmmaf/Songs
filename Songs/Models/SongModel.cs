40d173a64e6cc91770b5f86a88697c790524d450
Emma Forslund <emfo2102@student.miun.se>
2023-02-16 17:22:01 +0100

    Add project files.

---
+++ Songs/Models/SongModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Songs.Models
+{
+    public class SongModel
+    {
+        public int Id { get; set; } //PK
+
+        [Required]
+        public string? Artist { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        [Required]
+        public int Length { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+    }
+}
